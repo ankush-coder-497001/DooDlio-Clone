12f4e9cb6bd7c1fd0a13ed6bfc76eb00b2b45408
Ankush Kumar Gupta <167736782+ankush-coder-497001@users.noreply.github.com>
2025-03-13 22:39:25 +0530

    first commit

---
+++ client-side/src/components/Hero/Hero.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <section className="hero-section">
+        <div className="container">
+          <div className="hero-details">
+            <h1>Connect with your customers</h1>
+            <p>Use our template to quickly and efficiently brand your growing business.</p>
+            <div className="hero-buttons-wrap">
+              <a href="/" className="button">Preview Template</a>
+              <a href="/admin/licensing" className="button outline">Learn More</a>
+            </div>
+          </div>
+          <div className="hero-image-wrapper">
+            <img
+              src="https://cdn.prod.website-files.com/66b74339fc387196ca3341f8/66d41e81b5bebfb7d648c79f_hero-illustration-v2.svg"
+              alt="Hero Illustration"
+              className="hero-image"
+            />
+          </div>
+        </div>
+      </section>
+  )
+}
+
+export default Hero
---
+++ client-side/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header className="header">
+    <div className="container">
+      <nav className="navigation-wrapper">
+        <div className="navigation">
+          <div className="navigation-left">
+            <a href="#features" className="navigation-link">
+              <span className="link-text">Features</span>
+            </a>
+            <a href="#how-it-works" className="navigation-link">
+              <span className="link-text">How it Works</span>
+            </a>
+            <a href="#integrations" className="navigation-link">
+              <span className="link-text">Integrations</span>
+            </a>
+          </div>
+          <div className="navigation-middle">
+            <a href="/" className="logo-link">
+              <img
+                src="https://cdn.prod.website-files.com/66b74339fc387196ca3341f8/66b74696ae40a0a4ef3eabf2_Logo.svg"
+                alt="Logo"
+                className="doodlio-logo"
+              />
+            </a>
+          </div>
+          <div className="navigation-right">
+            <nav className="navigation-menu">
+              <a href="#faq" className="navigation-link">FAQ</a>
+              <a href="#team" className="navigation-link">Team</a>
+              <a href="#pricing" className="navigation-link">Pricing</a>
+            </nav>
+            <a
+              href="https://webflow.com/dashboard/marketplace-checkout?workspace=toms-design&p=6709fb0da04d688b7962788e"
+              className="small-button light"
+            >
+              Buy Template
+            </a>
+          </div>
+        </div>
+      </nav>
+    </div>
+  </header>
+  )
+}
+
+export default Navbar
